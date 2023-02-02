862588af7d9fe1e197aa6a46ba5a1a6bd5a19e23
Clefly <85061917+Clefly20@users.noreply.github.com>
2023-02-02 21:33:59 +0300

    Add files via upload

---
+++ Serial Communication/Serial Communication.ino
@@ -0,0 +1,33 @@
+
+
+void setup() {
+  Serial.begin(9600); //Seri iletişimi başlatıyoruz
+
+  delay(2000); //2 saniye bekliyoruz
+
+  Serial.println("Komutlar (Selam,Nasılsın)");
+}
+
+void loop() {
+
+  if (Serial.available()) {    //Eğer Serial Aktifse
+    String command;
+    command = Serial.readStringUntil('\n');  //Serial.read ı komuta eşitle
+    command.trim();                          //Fazladan karakterleri sil
+
+
+    if (command.equals("Selam")) {       //Eğer komut Selam a eşitse
+    Serial.println("Merhaba");
+    }
+
+    else if (command.equals("Nasılsın")) { //Eğer komut Nasılsın a eşitse
+    Serial.println("Iyiyim");
+    }
+
+    else {                              //Hiçbir komuta eşit değilse
+      Serial.println("Yanlış komut");
+    }
+    Serial.print("Komutunuz: ");
+    Serial.println(command);            //En son giren komutu yazdır
+  }
+}
